82aa5d8a515b919f6616abb36dd6a830f409ad86
Khanh Nguyen <khnguyen21@ssis.edu.vn>
2020-03-11 19:46:47 +0700

    ok

---
+++ Novel.py
@@ -0,0 +1,85 @@
+from tkinter import *
+import tkinter as tk
+from tkinter import messagebox
+from datetime import datetime, date
+#Functions
+import sqlite3 as sq
+con = sq.connect("/Users/apple/Documents/NovelApp/DBNovel.db")
+c = con.cursor()
+
+def getNovel():
+    novel = c.execute("SELECT Title, AuthorName FROM Book, Author, Wrote WHERE Book.ISBN = Wrote.ISBN AND Wrote.AuthorID = Author.AuthorID")
+    data = c.fetchall()
+    return data
+
+def insertNovel(NovelName, Author, BookPrice, BookGenre):
+    try:
+        insertAuthor = c.execute("INSERT INTO Author(AuthorName)Values(?)",(str(Author),))
+        insertNovel = c.execute("INSERT INTO Book(Title, Price, Genre) Values(?, ?, ?)",(str(NovelName), int(BookPrice), str(BookGenre)))
+        insertWrote = c.execute("INSERT INTO Wrote(AuthorID, ISBN)Values((SELECT MAX(AuthorID) FROM AUTHOR), (SELECT MAX(ISBN) FROM Book))")
+        con.commit()
+    except ValueError:
+        messagebox.showinfo('Error', 'Price not numeric. Please fix them.')
+
+#UI
+def root():
+    window = Tk()
+    window.title("Novel Application")
+    window.geometry("200x100")
+    nList = Button(window, text="Novel List", command = listNovelWindow)
+    nList.pack()
+
+    inNovel = Button(window, text="Add Novel", command = addNovelWindow)
+    inNovel.pack()
+
+    ext = Button(window, text="Exit", command = lambda:endProgram(window))
+    ext.pack()
+    window.mainloop()
+
+def endProgram(w):
+    con.close()
+    w.destroy()
+
+def listNovelWindow():
+    novels = getNovel()
+    tbl = ""
+    for row in novels:
+        for field in row:
+            tbl += str(field)
+            tbl += ", "
+        tbl += "\n"
+        tbl += "\n"
+    messagebox.showinfo("Novel List", tbl)
+
+def addNovelWindow():
+    addNovel = Tk()
+    addNovel.title("Add Novel")
+    addNovel.geometry("200x300")
+
+    addNovelFrame = Frame(addNovel)
+    addNovelFrame.pack(side = LEFT)
+
+    novelTitle = tk.StringVar(addNovel)
+    novelAuthor = tk.StringVar(addNovel)
+    novelPrice = tk.StringVar(addNovel)
+    novelGenre = tk.StringVar(addNovel)
+    
+    lblT = Label(addNovelFrame, text = 'Enter a novel title').pack()
+    title = Entry(addNovelFrame, text = 'Novel Title Here', textvariable = novelTitle).pack()
+    
+    lblA = Label(addNovelFrame, text = 'Enter the author').pack()
+    author = Entry(addNovelFrame, text = 'Novel Author Here', textvariable = novelAuthor).pack()
+
+    lblP = Label(addNovelFrame, text = 'Enter the price').pack()
+    author = Entry(addNovelFrame, text = 'Novel Price Here', textvariable = novelPrice).pack()
+
+    lblG = Label(addNovelFrame, text = 'Enter the genre').pack()
+    author = Entry(addNovelFrame, text = 'Novel Genre Here', textvariable = novelGenre).pack()
+    
+    add = Button(addNovelFrame, text="Add",
+                 command = lambda: insertNovel(novelTitle.get(), novelAuthor.get(), novelPrice.get(), novelGenre.get())).pack()
+    
+    addNovel.mainloop()
+
+root()
+
